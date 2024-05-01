cb8259da3a1f43d22131f813f251304d61d5f9e7
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-05-01 18:28:09 +0900

    Time: 43 ms (17.32%), Space: 16.7 MB (11.15%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -3,11 +3,9 @@ class Solution:
         left, right = 0, len(nums)-1
         while left <= right:
             if nums[left] == val:
-                print("val", left, right, end=" ")
                 nums[left], nums[right] = nums[right], nums[left]
                 right -= 1
             else:
-                print("not val", left, right, end=" ")
                 left += 1
             print(nums)
         return left
