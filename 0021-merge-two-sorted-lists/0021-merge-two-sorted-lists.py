08a04f428895b01b45ce76ab6526df7bc2b6df74
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-05-01 15:59:44 +0900

    Time: 37 ms (64.83%), Space: 16.6 MB (58.76%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -4,7 +4,7 @@ class ListNode:
         self.next = next
 
 def ListToSLL(l):
-    if not l:  # 입력 리스트가 비어있는 경우 처리
+    if not l:
         return None
     head = ListNode(l[0])
     curr = head
@@ -15,9 +15,6 @@ def ListToSLL(l):
 
 class Solution:
     def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
-        # 이미 ListNode이기 때문에 ListToSLL 호출이 필요 없다면 삭제
-        # list1 = ListToSLL(list1)
-        # list2 = ListToSLL(list2)
         dummy = ListNode()
         curr = dummy
         while list1 and list2:
@@ -28,7 +25,6 @@ class Solution:
                 curr.next = list2
                 list2 = list2.next
             curr = curr.next
-        # 남아있는 리스트 처리
         if list1:
             curr.next = list1
         elif list2:
