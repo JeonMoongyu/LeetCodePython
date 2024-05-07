b4f82df1ca3ce6deac62c0ced22f655289e5046d
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-05-07 19:00:20 +0900

    Time: 39 ms (66.55%), Space: 16.7 MB (52.16%) - LeetHub

---
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isMajorityElement(self, nums: List[int], target: int) -> bool:
+        cnt = 0
+        for num in nums:
+            if num == target:
+                cnt += 1
+        return cnt > len(nums) // 2
