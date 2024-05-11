e083f47f320d3221d3a3bba3ae3bc6ce5ea5dfe7
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-05-11 12:55:28 +0900

    Time: 72 ms (5.36%), Space: 16.7 MB (15.59%) - LeetHub

---
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        l = len(words)
+        out = []
+        for i in range(l):
+            for j in range(l):
+                if i != j and words[i] not in out and self.isSubstring(words[i], words[j]):
+                    out.append(words[i])
+        return out
+        
+    def isSubstring(self, word1: str, word2: str) -> bool:
+        len1, len2 = len(word1), len(word2)
+        if len1 > len2:
+            return False
+        for i in range(len2 - len1 + 1):
+            if word1 == word2[i:i+len1]:
+                return True
+        return False
+            
+        
