d8a83542ddb1d7c4da6c15db71f6131b5db0c02e
JeonMoongyu <156481357+JeonMoongyu@users.noreply.github.com>
2024-05-24 21:23:59 +0900

    Time: 45 ms (7.41%), Space: 16.5 MB (93.48%) - LeetHub

---
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.py
@@ -0,0 +1,13 @@
+class Solution:
+    def digitCount(self, num: str) -> bool:
+        cnt = {}
+        for i in num:
+            if int(i) in cnt:
+                cnt[int(i)] += 1
+            else:
+                cnt[int(i)] = 1
+        print(cnt)
+        for i in range(len(num)):
+            if (i in cnt and int(num[i]) != cnt[i]) or (i not in cnt and int(num[i]) != 0):
+                return False
+        return True
